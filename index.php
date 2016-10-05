a676a9bdafdbddc4d69d0537c4c0ea79c560cf19
jellytran80 <jellytran80@yahoo.com>
2016-10-05 15:38:32 +0700

    Barcode Generator PNG
    Generator Barcode 39 to PNG

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Setup Barcode Generator
+ * Type Code_39
+ * @param string $text [<text generator>]
+ * getBarcode(text,type,width,height)
+ */
+require('BarcodeGeneratorPNG.php');
+
+$text = 'BarcodeGeneratorPNG-Code39'; 
+$generator = new BarcodeGeneratorPNG();
+$imgbar = base64_encode($generator->getBarcode($text, $generator::TYPE_CODE_39, 1, 50));
+?>
+
+<?= '<img src="data:image/png;base64,' . $imgbar . '">'; ?>
+<h2><?= $text ?></h2>
