f70e2d23cf008c8d6a9433296094a0da04744894
jellytran80 <jellytran80@yahoo.com>
2016-10-07 13:26:15 +0700

    Barcode JPG
    Create images jpg

---
+++ BarcodeGeneratorJPG.php
@@ -0,0 +1,10 @@
+<?php 
+require "BarcodeGenerator.php";
+
+class BarcodeGeneratorJPG extends BarcodeGenerator {
+	//do something here
+	public function getBarcode($code, $type, $widthFactor = 2, $totalHeight = 30, $color = array(0, 0, 0)) {
+		
+	}
+}
+ ?>
