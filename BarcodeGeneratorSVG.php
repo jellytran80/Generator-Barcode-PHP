43fe4300c6d3af729e25adc5f0ac68e7ba2d5b27
jellytran80 <jellytran80@yahoo.com>
2016-10-18 09:53:20 +0700

    test
    Test

--- BarcodeGeneratorSVG.php
+++ BarcodeGeneratorSVG.php
@@ -3,6 +3,9 @@
  * Return SVG format
  */
 class BarcodeGeneratorSVG extends BarcodeGenerator{
-
+	
+	public function getBarcode($code, $type, $widthFactor = 2, $totalHeight = 30, $color = array(0, 0, 0)) {
+			
+	}
 }
  ?>
