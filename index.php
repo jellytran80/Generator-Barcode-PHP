9b4ef5967619540f80a70992ccf3f33c10cd6b65
jellytran80 <jellytran80@yahoo.com>
2016-10-19 17:12:26 +0700

    Barcode PNG size
    Add Image PNG with size 2

--- index.php
+++ index.php
@@ -11,8 +11,12 @@ require('BarcodeGeneratorPNG.php');
 $text = 'BarcodeGeneratorPNG-Code39'; 
 $generator = new BarcodeGeneratorPNG();
 $imgbar = base64_encode($generator->getBarcode($text, $generator::TYPE_CODE_39, 1, 50));
+$imgbar2 = base64_encode($generator->getBarcode($text, $generator::TYPE_CODE_39, 2, 50));
 ?>
 <div>
 <?= '<img src="data:image/png;base64,' . $imgbar . '">'; ?>
 <p><?= $text ?></p>	
 </div>
+<br>
+<?= '<img src="data:image/png;base64,' . $imgbar2 . '">'; ?>
+<p><?= $text ?></p>	
