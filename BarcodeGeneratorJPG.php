36ad0100588058bf3e592f012c33861db0a5eb15
jellytran80 <jellytran80@yahoo.com>
2016-10-26 09:54:09 +0700

    Generation JPG
    This is change for JPG

--- BarcodeGeneratorJPG.php
+++ BarcodeGeneratorJPG.php
@@ -4,7 +4,10 @@ require "BarcodeGenerator.php";
 class BarcodeGeneratorJPG extends BarcodeGenerator {
 	//do something here
 	public function getBarcode($code, $type, $widthFactor = 2, $totalHeight = 30, $color = array(0, 0, 0)) {
-		
+		$barcodeData = $this->getBarcodeData($code, $type);
+        // calculate image size
+        $width = ($barcodeData['maxWidth'] * $widthFactor);
+        $height = $totalHeight;	
 	}
 }
  ?>
